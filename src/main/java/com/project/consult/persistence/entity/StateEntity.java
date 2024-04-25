0130cf2abc3b521e10691ff4fbe56d204b04cfd0
santiagopereira12 <spereira98@misena.edu.co>
2024-04-25 15:40:45 -0500

    Generación de Entitys para definición de tablas y sus columnas en la base de datos

--- src/main/java/com/project/consult/persistence/entity/StateEntity.java
+++ src/main/java/com/project/consult/persistence/entity/StateEntity.java
@@ -1,2 +1,16 @@
-package com.project.consult.persistence.entity;public class StateEntity {
+package com.project.consult.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "state")
+public class StateEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idState;
+    @Column(name = "id_user")
+    private Integer idUser;
+    @Column(name = "state")
+    private boolean state;
 }
--- src/main/java/com/project/consult/persistence/entity/UserEntity.java
+++ src/main/java/com/project/consult/persistence/entity/UserEntity.java
@@ -1,2 +1,22 @@
-package com.project.consult.persistence.entity;public class UserEntity {
+package com.project.consult.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUser;
+    @Column(name = "ndoc")
+    private Integer documento;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "apellido")
+    private String apellido;
+    @Column(name = "date_agregado")
+    private Date dtAgregado;
 }
